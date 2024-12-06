d96c7012604113dc2a4cd2f572c2fa955db02973
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-06 13:10:07 -0800

    task: option checkbox event handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'pwd';
+  password = '';
+  useLetters = false;
+  handleUseLetters = () => (this.useLetters = !this.useLetters);
+
+  useNumbers = false;
+  handleUseNumbers = () => (this.useNumbers = !this.useNumbers);
+
+  useSymbols = false;
+  handleUseSymbols = () => (this.useSymbols = !this.useSymbols);
+
+  /**Generate password.*/
+  onButtonClick() {
+    console.log(this.useLetters);
+    console.log(this.useNumbers);
+    console.log(this.useSymbols);
+    this.password = 'Password!!!';
+  }
 }
