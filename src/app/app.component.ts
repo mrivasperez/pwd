c201201f42c79e30dd774ec2b01cd3e5c93eee19
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-12-06 14:18:11 -0800

    task: render generated password conditionally - if pwd is generated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [CommonModule],
 })
 export class AppComponent {
   title = 'pwd';
